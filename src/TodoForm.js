17177343a6f2128e996cf9033e923ce28ed048c3
Appsius <ilyas.ustunkaya234434@gmail.com>
2020-01-09 22:25:07 +0300

    Wrong

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -2,11 +2,11 @@ import React, { useContext } from 'react';
 import { TextField } from '@material-ui/core';
 import { Paper } from '@material-ui/core';
 import useInputState from './hooks/useInputState';
-import { TodosContext } from './context/todos.context';
+import { DispatchContext } from './context/todos.context';
 
 function TodoForm() {
   const [value, handleChange, reset] = useInputState();
-  const { dispatch } = useContext(TodosContext);
+  const { dispatch } = useContext(DispatchContext);
   return (
     <Paper style={{ margin: '1rem 0', padding: '0 1rem' }}>
       <form
--- src/context/todos.context.js
+++ src/context/todos.context.js
@@ -1,6 +1,5 @@
 import React, { createContext, useReducer } from 'react';
 import todoReducer from '../reducers/todo.reducer';
-import useTodoState from '../hooks/useTodoState';
 
 const defaultTodos = [
   { id: 1, task: 'Lay down the gun!!', completed: false },
@@ -8,12 +7,13 @@ const defaultTodos = [
 ];
 
 export const TodosContext = createContext();
+export const DispatchContext = createContext();
 
 export function TodosProvider(props) {
   const [todos, dispatch] = useReducer(todoReducer, defaultTodos);
   return (
-    <TodosContext.Provider value={{ todos, dispatch }}>
-      {props.children}
+    <TodosContext.Provider value={{ todos }}>
+      <DispatchContext value={{ dispatch }}>{props.children}</DispatchContext>
     </TodosContext.Provider>
   );
 }
